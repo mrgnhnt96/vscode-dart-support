d45578471384a6f43dbfeaea9c33154108a3a1ad
Morgan Hunt <mrgnhnt96@gmail.com>
2021-11-02 09:29:28 -0500

    feat: create add setting & read setting

---
+++ src/helpers/vscode_helper.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+export function readSetting(key: string) {
+  return vscode.workspace.getConfiguration().get("dbr." + key);
+}
+
+export function addSetting(key: string, value: any) {
+  return vscode.commands.executeCommand("setContext", key, value);
+}
