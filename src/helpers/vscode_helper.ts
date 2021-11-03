72af30e2d54b849197504deb1b98f863023b11f9
Morgan Hunt <mrgnhnt96@gmail.com>
2021-11-03 13:34:45 -0500

    feat: improve settings by referencing the packageName

--- src/helpers/vscode_helper.ts
+++ src/helpers/vscode_helper.ts
@@ -1,11 +1,16 @@
 import * as vscode from "vscode";
+import { packageName } from "../extension";
 
 export function readSetting(key: string) {
-  return vscode.workspace.getConfiguration().get("dbr." + key);
+  return vscode.workspace.getConfiguration().get(`${packageName}.` + key);
 }
 
 export function addSetting(key: string, value: any) {
-  return vscode.commands.executeCommand("setContext", key, value);
+  return vscode.commands.executeCommand(
+    "setContext",
+    `${packageName}.` + key,
+    value
+  );
 }
 
 export function notify(message: string) {
